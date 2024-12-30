3035558f7c005bde120431322e7c0db0f270cbc8
fauzi niko <fauziniko12@gmail.com>
2024-12-30 23:45:50 +0700

    coba-fe

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function showContent(sectionId) {
+    // Hide all sections
+    const sections = document.querySelectorAll('.content-section');
+    sections.forEach((section) => {
+      section.classList.remove('active');
+    });
+  
+    // Show the selected section
+    const activeSection = document.getElementById(sectionId);
+    if (activeSection) {
+      activeSection.classList.add('active');
+    }
+  }
+  
+  // Initialize the default section
+  document.addEventListener('DOMContentLoaded', () => {
+    showContent('home');
+  });
+  
